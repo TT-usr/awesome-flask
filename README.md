c022e4ad6cce296abc008ea5a90a2b299f22438a
JackYao <yaotiancheng@geekpark.net>
2017-11-21 18:03:45 +0800

    html add obj to mongo

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # awesome-flask
 
+###运行环境要求
+`MongoDB`
+安装可以使用 HomeBrew 一键安装
+`brew install mongodb`
+启动本地 MongoDB 服务
+`mongod`
+
 ###安装依赖
 `pip install -r requirenments.txt`
+
+###运行项目
+`python manage.py runserver`
+
--- app/views.py
+++ app/views.py
@@ -3,8 +3,18 @@
 from app import app
 from flask import render_template
 from app.models import Todo
+# 这个包还得导入,日了蛋
+from flask import request
 
 @app.route('/')
 def index():
+	todos = Todo.objects.all()
+	return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST',])
+def add():
+	content = request.form.get("content")
+	todo = Todo(content=content)
+	todo.save()
 	todos = Todo.objects.all()
 	return render_template('index.html', todos=todos)
