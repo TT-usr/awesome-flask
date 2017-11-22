491ddc59bc37bffb3c2daa57f044c37114d0623f
JackYao <yaotiancheng@geekpark.net>
2017-11-22 16:47:20 +0800

    add bootstrap

--- app/models.py
+++ app/models.py
@@ -2,10 +2,14 @@
 
 from app import db
 import datetime
+from flask_mongoengine.wtf import model_form
+
 
 class Todo(db.Document):
  	content = db.StringField(required=True, max_length=20)
  	time = db.DateTimeField(default=datetime.datetime.now())
  	status = db.IntField(default=0)
 
+# 通过Todo类生成TodoForm类, 数据库中的验证条件就和表单验证条件达成一致。
+TodoForm = model_form(Todo)
  				 		
--- app/views.py
+++ app/views.py
@@ -2,22 +2,60 @@
 
 from app import app
 from flask import render_template
-from app.models import Todo
+from app.models import Todo, TodoForm
 # 这个包还得导入,日了蛋
-from flask import request
+from flask import request, current_app, make_response, redirect, abort
+# 导入 bootstrap
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap(app)
+
+# 动态路由
+@app.route('/user/<name>')
+def user(name):
+	# 打印请求头
+	# print(request.headers)
+
+	# 程序上下文
+	# print(current_app.name)
+
+	# 程序所有的路由
+	# print(app.url_map)
+	
+	# return '<h1>hello, %s</h1>' % name
+
+	# 第二个参数是 http 状态码
+	# return '我是404哦', 404
+
+	# 还可以返回 response
+	# response = make_response('<h1>hello, %s</h1>' % name)
+	# response.set_cookie('key','value')
+	# return response
+
+	# 重定向
+	# return redirect('http://www.baidu.com')
+
+	# abort 不会把控制权交还给调用它的函数，而是抛出异常把控制权交给 Web 服务器
+	# abort(404)
+
+	return render_template('user.html',name=name)
+
 
 @app.route('/')
 def index():
+	form = TodoForm()
 	todos = Todo.objects.all()
-	return render_template('index.html', todos=todos)
+	return render_template('index.html', todos=todos, form=form)
 
 @app.route('/add', methods=['POST',])
 def add():
-	content = request.form.get("content")
-	todo = Todo(content=content)
-	todo.save()
+	form = TodoForm(request.form)
+	if form.validate_on_submit():
+		content = request.form.get("content")
+		todo = Todo(content=content)
+		todo.save()
 	todos = Todo.objects.all()
-	return render_template('index.html', todos=todos)
+	return render_template('index.html', todos=todos, form=form)
 
 @app.route('/done/<string:todo_id>', methods=['POST',])
 def done(todo_id):
